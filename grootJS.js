68ac83ed657600dd44064d993fd234ebfdc78f71
Bhushan Mendhe <bhushanmendhe15@gmail.com>
2021-09-17 10:16:31 +0530

    wired output box

--- grootJS.js
+++ grootJS.js
@@ -1,7 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
+var txtOutput = document.querySelector("#txt-output")
+
+console.log(txtOutput.innerText)
 
 btnTranslate.addEventListener("click", clickEventHandler => {
     console.log("clicked");
     console.log(txtInput.value);
 })
+
